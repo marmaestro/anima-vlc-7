fc4c5f06e99d3aff6774a587c75028b75a3bec77
AlbertoAlanSA <83225413+AlbertoAlanSA@users.noreply.github.com>
2024-09-18 10:37:13 +0200

    hover cromos + valencia

--- aima-vlc-7/scenes/card.gd
+++ aima-vlc-7/scenes/card.gd
@@ -1,16 +1,20 @@
+extends Area2D
+
 
 var _name : String = "pollo"
 var _spriteRoute : String = "res://sprites/birds/"
-@onready var sprite: Sprite2D = $Sprite #load(ruta)
+@onready var sprite: Sprite2D = $CardImage/Sprite
 var _description : String = "esta to guapo el pollo ese la verda"
-@onready var description: RichTextLabel = $Container/Description
+@onready var description: RichTextLabel = $CardImage/Container/Description
 var _neededObject : String = "palo" 
-@onready var collision_shape_2d: CollisionShape2D = $"../CollisionShape2D"
+@onready var size_animator: AnimationPlayer = $sizeAnimator
+var isBig : bool = false
+@onready var exit_button: Button = $ExitButton
 
 var _birds : Array = ["ànec collverd", "cotxa fumada", "gavià de potes grogues", "teuladí", "gafarró", "garsa comuna"]
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	exit_button.visible = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -26,4 +30,17 @@ func generateNewBird()-> void:
 	sprite.texture = load(_spriteRoute+_name+".png")
 	_description = "esta to guapo el "+_name+" la verda"
 	description.text=_description
-	
+			
+
+
+func _on_mouse_shape_entered(shape_idx: int) -> void:
+	if(!isBig):
+		isBig=true
+		exit_button.visible = true
+		size_animator.play("getBigger")
+
+
+func _on_exit_button_pressed() -> void:
+	isBig=false
+	exit_button.visible = false
+	size_animator.play("getSmaller")

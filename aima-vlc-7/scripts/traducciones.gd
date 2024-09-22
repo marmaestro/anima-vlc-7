d01e6ef0643e2f2229161aa0c303c9295d6c1e5a
Alberto Alan Sánchez <al404015@uji.es>
2024-09-22 22:06:53 +0200

    arreglado 1er objeto valenciano

--- aima-vlc-7/scripts/traducciones.gd
+++ aima-vlc-7/scripts/traducciones.gd
@@ -3,6 +3,7 @@ extends Node2D
 @onready var titol: Node2D = %Titol
 @onready var traducciones: Node2D = $"."
 @onready var button_sounds: AudioStreamPlayer2D = %ButtonSounds
+@onready var letter: Area2D = %Letter
 
 
 
@@ -19,9 +20,11 @@ func _on_valencia_pressed() -> void:
 	TranslationServer.set_locale("ca")
 	traducciones.visible=false
 	titol.visible =true
-
+	letter.generateLetter()
+	
 func _on_castellano_pressed() -> void:
 	button_sounds.play()
 	TranslationServer.set_locale("es")
 	traducciones.visible=false
 	titol.visible =true
+	letter.generateLetter()

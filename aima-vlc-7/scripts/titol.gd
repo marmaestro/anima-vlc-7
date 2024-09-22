ce34ab309038434e00ee2ecfa86570de7cb3aba4
Alberto Alan Sánchez <al404015@uji.es>
2024-09-22 20:15:43 +0200

    pantalla traducciones

--- aima-vlc-7/scripts/titol.gd
+++ aima-vlc-7/scripts/titol.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @onready var titol: Node2D = $"."
+@onready var game: Node2D = %Game
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -14,3 +15,4 @@ func _process(delta: float) -> void:
 
 func _on_button_pressed() -> void:
 	titol.visible=false
+	game.visible = true
